4997c5905e4b605b42dc304a3778b82e08b540a5
alvindioprakosa <alvindioprakosa@gmail.com>
2025-03-20 09:13:37 +0700

    Update Product

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -3,19 +3,19 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 const ProductPage = () => {
-    const [isLogin, setisLogin] = useState(false);
-    const {push} = useRouter();
+  const [isLogin, setisLogin] = useState(false);
+  const { push } = useRouter();
 
-    useEffect(() => {
-        if (!isLogin) {
-            push("/auth/login");
-        }
-    }, [])
-    return (
-        <div>
-            <h1>The product</h1>
-        </div>
-    )
-}
+  useEffect(() => {
+    if (!isLogin) {
+      push("/auth/login");
+    }
+  }, []);
+  return (
+    <div>
+      <h1>The product</h1>
+    </div>
+  );
+};
 
 export default ProductPage;
