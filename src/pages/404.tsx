ac55588bdf01c68359a315caec02431380c28bf6
alvindioprakosa <alvindioprakosa@gmail.com>
2025-05-03 14:25:07 +0700

    Update 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,23 @@
+// app/not-found.tsx
 import Link from 'next/link';
 
-const Custom404 = () => {
-    return (
-        <div className="flex justify-center items-center h-screen bg-gray-100">
-            <div className="text-center p-4">
-                <h1 className="text-4xl font-bold text-red-500">404</h1>
-                <p className="text-xl text-gray-700">Oops! The page you're looking for could not be found.</p>
-                <div className="mt-4">
-                    <Link href="/">
-                        <a className="text-blue-500 hover:underline">Go back to Home</a>
-                    </Link>
-                </div>
-            </div>
+export default function NotFound() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="text-center p-4">
+        <h1 className="text-4xl font-bold text-red-500">404</h1>
+        <p className="text-xl text-gray-700">
+          Oops! The page you're looking for could not be found.
+        </p>
+        <div className="mt-4">
+          <Link
+            href="/"
+            className="text-blue-500 hover:underline"
+          >
+            Go back to Home
+          </Link>
         </div>
-    );
-};
-
-export default Custom404;
+      </div>
+    </div>
+  );
+}
